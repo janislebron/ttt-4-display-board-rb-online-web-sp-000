1033ff03836a96a4ef920274a6d01a8a92b7dd54
Janis Lebron <janislebron@gmail.com>
2020-04-26 17:36:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
-board = [" "," "," "," "," "," "," "," "," "]
-
 def display_board(board)
-  puts "X: #{board[0]}"
-  puts "X: #{board[7]}"
-  puts "X: #{board[3]}"
-  puts "X: #{board[4]}"
-  puts "X: #{board[1]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
