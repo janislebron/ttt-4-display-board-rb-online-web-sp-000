cf45431b185afe2d1dfd65bd903dc40ed25eba7a
Janis Lebron <janislebron@gmail.com>
2020-04-26 17:35:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts "X: #{board[0]}"
+  puts "X: #{board[7]}"
+  puts "X: #{board[3]}"
+  puts "X: #{board[4]}"
+  puts "X: #{board[1]}"
+end
